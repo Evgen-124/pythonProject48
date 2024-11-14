fc260251f9b4ca8b101a141c45f9602a54a506db
EvgeniyPanov <Zhenya_panov_1994@mail.ru>
2024-11-14 19:11:47 +0300

    Update Module_6_1.py

--- Module_6_1.py
+++ Module_6_1.py
@@ -4,28 +4,26 @@ class Animal:
         self.alive = True
         self.fed = False
 
+    def eat(self, food):
+        if isinstance(food, Plant) and food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            if not isinstance(food, Plant):
+                print("Причина: передан не экземпляр Plant или его потомка.")
+            self.alive = False
+
 class Plant:
     def __init__(self, name):
         self.name = name
         self.edible = False
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 class Flower(Plant):
     pass
@@ -34,6 +32,7 @@ class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
         self.edible = True
+
 
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
@@ -46,8 +45,8 @@ print(p1.name)
 print(a1.alive)
 print(a2.fed)
 
-a1.eat(p1)
-a2.eat(p2)
+a1.eat(p1) 
+a2.eat(p2)  
 
-print(a1.alive)
-print(a2.fed)
+print(a1.alive)  
+print(a2.fed)    
